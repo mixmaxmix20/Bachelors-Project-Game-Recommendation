b49c5b23c871da6f61953b93833d5b2cb19f6570
Maksymilian Haliński <maxhalinski@gmail.com>
2026-08-13 14:33:52 +0200

    refactor(ui): rewrite Home page to Tailwind

--- src/components/layout/Button.tsx
+++ src/components/layout/Button.tsx
@@ -2,19 +2,28 @@ import React from "react";
 
 interface ButtonProps {
     onClickPar?: () => void,
+    isStandalone?: boolean,
     children?: React.ReactNode,
     isLogout?: boolean
 }
 
-function Button({ onClickPar, children, isLogout }: ButtonProps) {
-    const baseClasses = "flex items-center border-none text-white bg-[#1f2326] py-3 px-6 m-2 text-2xl rounded-lg cursor-pointer w-[88%] opacity-0 overflow-hidden transition-all duration-300 hover:scale-105 group-hover:opacity-100"
+function Button({ onClickPar, isStandalone, children, isLogout }: ButtonProps) {
+    let opacity, spanHidden;
+    if (isStandalone) {
+        opacity = "";
+        spanHidden = "";
+    } else {
+        opacity = "opacity-0 group-hover:opacity-100";
+        spanHidden = "hidden group-hover:inline-block"
+    }
+    const baseClasses = "flex items-center border-none text-white bg-[#1f2326] py-3 px-6 m-2 text-2xl rounded-lg cursor-pointer w-[88%] overflow-hidden transition-all duration-300 hover:scale-105"
     const colorClasses = isLogout ? "bg-[#1f2326] hover:bg-red-600" : "bg-[#1f2326] hover:bg-[#2b2d39]"
     return (
         <button
             onClick={onClickPar}
-            className={`${baseClasses} ${colorClasses}`}
+            className={`${baseClasses} ${colorClasses} ${opacity}`}
         >
-            <span className="hidden whitespace-nowrap group-hover:inline-block">{children}</span>
+            <span className={`whitespace-nowrap ${spanHidden}`}>{children}</span>
         </button>
     )
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,7 @@
 import { useNavigate } from "react-router-dom";
-import Navbar from "../../components/layout/Navbar";
-import "./Home.css";
+import Button from "../../components/layout/Button";
 
-interface HomeProps {}
-
-function Home(props: HomeProps) {
+function Home() {
   const navigate = useNavigate();
 
   function onButtonClickLogin() {
@@ -16,37 +13,20 @@ function Home(props: HomeProps) {
   }
 
   return (
-    <div className="mainContainer">
-      <div className="titleContainer">
+    <div className="mainContainer bg-linear-to-b from-[#1f1f1f] to-[#0d0d0d]">
+      <div className="titleContainer -mt-24 mb-12 text-center">
         <h2>SRG</h2>
       </div>
-      <div className="pContainer">
-        <p className="pHome">Witaj!</p>
-        <p className="pHome">
+      <div className="w-1/2 border-4 border-[#1f2326] bg-[#1f2326] mb-10">
+        <p className="text-2xl">Witaj!</p>
+        <p className="text-2xl">
           SRG to narzędzie do rekomendacji gier - dodaj swoje ulubione tytuły i
           odkryj nowe, dopasowane do twoich upodobań.
         </p>
       </div>
-      <div className="buttonRow">
-        <div className="buttonContainer">
-          {/* <p>Stwórz profil:</p> */}
-          <input
-            className="inputButton"
-            type="button"
-            onClick={onButtonClickRegister}
-            value="Stwórz profil"
-          ></input>
-        </div>
-        <br />
-        <div className="buttonContainer">
-          {/* <p>Zaloguj się:</p> */}
-          <input
-            className="inputButton"
-            type="button"
-            onClick={onButtonClickLogin}
-            value="Zaloguj się"
-          ></input>
-        </div>
+      <div className="flex justify-center gap-24 mt-5">
+        <Button onClickPar={onButtonClickRegister} isStandalone={true}>Stwórz profil</Button>
+        <Button onClickPar={onButtonClickLogin} isStandalone={true}>Zaloguj się</Button>
       </div>
     </div>
   );
