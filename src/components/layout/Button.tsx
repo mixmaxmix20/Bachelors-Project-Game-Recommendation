097516aa07efbe09a5c0dfd1ebc719e646abf7f6
Maksymilian Haliński <maxhalinski@gmail.com>
2026-08-22 14:18:21 +0200

    style(navbar): improve button hover animations

--- src/components/layout/Button.tsx
+++ src/components/layout/Button.tsx
@@ -13,10 +13,10 @@ function Button({ onClickPar, isStandalone, children, isLogout }: ButtonProps) {
         opacity = "";
         spanHidden = "";
     } else {
-        opacity = "opacity-0 group-hover:opacity-100 w-[88%]";
-        spanHidden = "hidden group-hover:inline-block"
+        opacity = "opacity-0 group-hover:opacity-100 w-[88%] pointer-events-none group-hover:pointer-events-auto";
+        spanHidden = "inline-block"
     }
-    const baseClasses = "flex items-center border-none text-white bg-[#1f2326] py-3 px-6 m-2 text-2xl rounded-lg cursor-pointer overflow-hidden transition-all duration-300 hover:scale-105 hover:border-solid hover:border-1 border-lightgrey"
+    const baseClasses = "flex items-center border border-transparent text-white bg-[#1f2326] py-3 px-6 m-2 text-2xl rounded-lg cursor-pointer overflow-hidden transition-all duration-300 hover:scale-105 hover:border-gray-400"
     const colorClasses = isLogout ? "bg-[#1f2326] hover:bg-red-600" : "bg-[#1f2326] hover:bg-[#2b2d39]"
     return (
         <button
